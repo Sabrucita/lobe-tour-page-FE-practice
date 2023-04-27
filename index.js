ce0cbe228ec0fd3ad8ff5370d81f3ebca0fb7af1
Sabrina <pereira.tsabrina@gmail.com>
2023-04-27 18:39:50 -0300

    LT: js style added

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu')
+const closeMenu = document.getElementById('closeMenu')
+const navLinks = document.getElementById('navLinks')
+closeMenu.classList.toggle('hidden',true)
 
 const openSidebar = hamburgerMenu.addEventListener('click', ()=>{
-  
+  if(navLinks.classList.contains('hidden')){
+    hamburgerMenu.classList.toggle('hidden',true)
+    closeMenu.classList.toggle('hidden',false)
+    navLinks.classList.toggle('hidden',false)
+  }
+})
+
+const closeSidebar = closeMenu.addEventListener('click', ()=>{
+  if(!navLinks.classList.contains('hidden')){
+    closeMenu.classList.toggle('hidden',true)
+    hamburgerMenu.classList.toggle('hidden',false)
+    navLinks.classList.toggle('hidden',true)
+  }
 })

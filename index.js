7dedb3893155d062b5fd279ca02e08a7d1d0af13
Sabrina <pereira.tsabrina@gmail.com>
2023-04-28 17:21:18 -0300

    LT: fixed download button in nav

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu')
 const closeMenu = document.getElementById('closeMenu')
 const navLinks = document.getElementById('navLinks')
+const navBtn = document.getElementById('menuDownloadBtn')
 
 hamburgerMenu.classList.toggle('hidden',false)
 closeMenu.classList.toggle('hidden',true)
@@ -10,10 +11,12 @@ const openSidebar = hamburgerMenu.addEventListener('click', ()=>{
   hamburgerMenu.classList.toggle('hidden',true)
   closeMenu.classList.toggle('hidden',false)
   navLinks.classList.toggle('hidden',false)
+  navBtn.classList.toggle('hidden',false)
 })
 
 const closeSidebar = closeMenu.addEventListener('click', ()=>{
   closeMenu.classList.toggle('hidden',true)
   hamburgerMenu.classList.toggle('hidden',false)
   navLinks.classList.toggle('hidden',true)
+  navBtn.classList.toggle('hidden',true)
 })

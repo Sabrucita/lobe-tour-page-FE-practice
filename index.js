d7c31c8fa53ad9dfe56d2f02f1db2d320bc1ae0c
Sabrina <pereira.tsabrina@gmail.com>
2023-04-28 15:57:47 -0300

    LT: finish style on nav

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu')
 const closeMenu = document.getElementById('closeMenu')
 const navLinks = document.getElementById('navLinks')
+
+hamburgerMenu.classList.toggle('hidden',false)
 closeMenu.classList.toggle('hidden',true)
+navLinks.classList.toggle('hidden',false)
 
 const openSidebar = hamburgerMenu.addEventListener('click', ()=>{
-  if(navLinks.classList.contains('hidden')){
-    hamburgerMenu.classList.toggle('hidden',true)
-    closeMenu.classList.toggle('hidden',false)
-    navLinks.classList.toggle('hidden',false)
-  }
+  hamburgerMenu.classList.toggle('hidden',true)
+  closeMenu.classList.toggle('hidden',false)
+  navLinks.classList.toggle('hidden',false)
 })
 
 const closeSidebar = closeMenu.addEventListener('click', ()=>{
-  if(!navLinks.classList.contains('hidden')){
-    closeMenu.classList.toggle('hidden',true)
-    hamburgerMenu.classList.toggle('hidden',false)
-    navLinks.classList.toggle('hidden',true)
-  }
+  closeMenu.classList.toggle('hidden',true)
+  hamburgerMenu.classList.toggle('hidden',false)
+  navLinks.classList.toggle('hidden',true)
 })
